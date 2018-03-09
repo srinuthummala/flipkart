0f8f485bdd796c5980521bc0a57965d5bfcf35f4
srinuthummala <srieceds@gmail.com>
2018-03-09 17:19:51 +0530

    project added

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,48 @@
+package pages;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import base.TestBase;
+import utility.Utility;
+
+public class LoginPage extends TestBase {
+	public static Logger log = Utility.getLogger(LoginPage.class);
+
+	
+	@FindBy(xpath="//span[contains(text(), 'Enter Email/Mobile number')]/../parent::div/input")
+	WebElement txtUsername;
+	
+	@FindBy(xpath="//span[contains(text(), 'Enter Password')]/../parent::div/input")
+	WebElement txtPassword;
+	
+	@FindBy(xpath="//span[contains(text(), 'Login')]/ancestor::button")
+	WebElement btnLogin;
+	
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+		
+	public HomePage login(String uname, String pass) {
+		
+		log.info("**********started login functionality***************");
+		log.info("entering username :  "+uname);
+		setText(txtUsername, uname);
+		log.info("entering password :  "+pass);
+		setText(txtPassword, pass);
+		click(btnLogin);
+		
+		log.info("************* login functionality done *****************");
+
+		return new HomePage();
+	}
+	
+	public String getLoginPageTitle() {
+		return driver.getTitle();
+	}
+
+}
